d98b169d018228a09f693167a07eb120edcb2b47
Luis_Raul_Arzola_Torres <a171845@une.edu.mx>
2019-06-14 01:43:57 -0500

    Hola

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Me Perdonaz?</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <h1>Esto no es un sitio de posts</h1>
+
+    <div class="container">
+        <div class="container2">
+
+            <div class="btnCerrar">
+                <input type="button" value="X">
+            </div>
+
+            <div class="titulo">
+                Título
+            </div>
+
+            <div class="contenido">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque iste veniam non deserunt suscipit repellendus iusto vero accusantium, doloremque, architecto ab corrupti nobis quam reiciendis fugit explicabo tenetur aliquam modi!
+            </div>
+
+            <div class="fecha">
+                21/04/2019
+            </div>
+        </div>
+
+        <div class="container2">
+
+            <div class="btnCerrar">
+                <input type="button" value="X">
+            </div>
+
+            <div class="titulo">
+                Título
+            </div>
+
+            <div class="contenido">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque iste veniam non deserunt suscipit repellendus iusto vero accusantium, doloremque, architecto ab corrupti nobis quam reiciendis fugit explicabo tenetur aliquam modi!
+            </div>
+
+            <div class="fecha">
+                21/04/2019
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
