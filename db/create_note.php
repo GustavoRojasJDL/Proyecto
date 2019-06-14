1a1d10d3fd0ae2c68946971805624676e8640cd8
Gustavo Rojas <jgdlr99@hotmail.com>
2019-06-14 14:00:46 -0500

    Update create_note.php

--- db/create_note.php
+++ db/create_note.php
@@ -13,11 +13,33 @@ if(empty($_POST)){
   $sec = $time["seconds"];
   $date = $year."-".$month."-".$day." ".$hour.":".$min.":".$sec;
   echo $date;
+  $tl = $_POST["title"];
+  $dc = $_POST["desc"];
 
-  $sql = "INSERT INTO `notes` (`title`, `description`, `date`, `active`) VALUES (NULL, NULL, '$date', '1')";
+  $sql = "INSERT INTO `notes` (`title`, `description`, `date`, `active`) VALUES ('$tl', '$dc', '$date', '1')";
   $result = $conn->query($sql);
 
   header('Location: '."./");
 }
 ?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="../css/style.css">
+  <title>Document</title>
+</head>
+<body>
+  <form action="" method="post">
+    <div class="form_note">
+      <input type="text" name="title">
+      <textarea name="desc" cols="30" rows="10"></textarea>
+    </div>
+    <div class="form_btn">
+      <button type="submit">Guardar</button>
+    </div>
+  </form>
+</body>
+</html>
