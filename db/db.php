e36c1e5689544bdd286ef3686156501e1cf9a5a9
Luis_Raul_Arzola_Torres <a171845@une.edu.mx>
2019-06-18 20:24:26 -0500

    ya

--- db/db.php
+++ db/db.php
@@ -1,11 +1,18 @@
 <?php
-  $servername = "localhost";
-  $username = "root";
-  $password = "mysql";
+  $servername = 'localhost';
+  $username = 'root';
+  $password = 'mysql';
   $db = "Proyecto";
 
   $conn = new mysqli($servername, $username, $password, $db);
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->xconnect_error);
   }
+
+  // $servername = "localhost";
+  // $username = "root";
+  // $password = "mysql";
+
+  // // Create connection
+  // $conn = new mysqli($servername, $username, $password, "Proyecto");
 ?>
--- index.php
+++ index.php
@@ -6,15 +6,24 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Me Perdonaz?</title>
+  <script type="text/javascript">
+    window.onload = function(){
+      document.onmousemove = function(ev){
+        var x = -ev.clientX / 10;
+        var y = -ev.clientY / 10;
+        document.body.style.backgroundPosition = x + 'px ' + y + 'px';
+      };
+    };
+    </script>
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 </head>
 
-<body>
+<body background="/Proyecto/images/galaxy.jpg" style="background-repeat: no-repeat; background-position: center; background-attachment: fixed;">
   <div class="container_add">
     <a class="add" href="db/create_note.php"><i class="fas fa-plus-circle"></i></a>
   </div>
-  <h1>Esto no es un sitio de posts</h1>
+  <h1 class="caja">Esto no es un sitio de posts</h1>
   <div class="container"><?php require_once("./db/read_notes.php"); ?></div>
 </body>
 
